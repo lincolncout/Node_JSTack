8fd507d6283f504277d7e33b214132cb2e1cfa34
lincolncout <lincolnanc@gmail.com>
2023-08-02 14:05:11 -0300

    ultimos ajustes

--- mycontacts/.eslintrc.js
+++ mycontacts/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'consistent-return': 'off',
     'import/no-extraneous-dependencies': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+require('express-async-errors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json()); // faz o parse do json do body quando por exemplo ocorre um evento post
-
 app.use(routes);
+
+// error handler precisa vir depois das definições da rota
+app.use((error, request, response, next) => {
+  console.log('##### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log(' Server started at http://localhost:3000 '));
 
