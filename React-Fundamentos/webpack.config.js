facaef3253bf8d66e0f0b9fd833af8469015c6d3
Lincoln Antunes Nogueira Coutinho <lincolnantunesnogueiracoutinho@MacBook-Air-de-Lincoln.local>
2024-09-25 21:12:32 -0300

    alteracao no webpack

--- React-Fundamentos/webpack.config.js
+++ React-Fundamentos/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
-// path.resolve(), monta o path de acordo com sistema operacional podendo ser src/ ou src\ a depender do sistema operacional
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'), // diretorio, pasta, arquivo 
+  entry: path.resolve(__dirname, 'src', 'index.js'), 
+  output: {
+    path: path.resolve(__dirname, 'build')
+  }
 }
